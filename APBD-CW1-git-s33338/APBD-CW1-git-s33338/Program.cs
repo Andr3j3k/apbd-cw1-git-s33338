e66397ed8ca1604fbef6e948b9d06afe01e3b9ec
s33338 <s33338@pjwstk.edu.pl>
2026-03-12 13:26:04 +0100

    Komunikat Startowy

--- APBD-CW1-git-s33338/APBD-CW1-git-s33338/Program.cs
+++ APBD-CW1-git-s33338/APBD-CW1-git-s33338/Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Program do prostych obliczeń");
+Console.Write("Podaj liczbę: ");
+String input = Console.ReadLine();
+Console.WriteLine($"Wpisałeś: {input}");
