a7840bd66500b5205e8a7f9f5aeb385edc6f0ee0
s33338 <s33338@pjwstk.edu.pl>
2026-03-12 13:30:09 +0100

    Poprawa komunikatów

--- APBD-CW1-git-s33338/APBD-CW1-git-s33338/Program.cs
+++ APBD-CW1-git-s33338/APBD-CW1-git-s33338/Program.cs
@@ -1,14 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Program do prostych obliczeń");
-Console.Write("Podaj liczbę: ");
+using APBD_CW1_git_s33338;
+
+Console.WriteLine("=== Program do prostych obliczeń ===");
+Console.Write("Podaj liczbę całkowitą: ");
 String input = Console.ReadLine();
  
 if (int.TryParse(input, out int number))
 {
-    Console.WriteLine($"Podano poprawną liczbę: {number}");
+    Console.WriteLine();
+    Console.WriteLine("Wyniki obliczeń:");
+    Console.WriteLine($"Liczba: {number}");
+    Console.WriteLine($"Kwadrat: {StatisticsHelper.Square(number)}");
+    Console.WriteLine($"Sześcian: {StatisticsHelper.Cube(number)}");
 }
 else
 {
-    Console.WriteLine("Błąd: podana wartość nie jest liczbą całkowitą.");
+    Console.WriteLine();
+    Console.WriteLine("Błąd: musisz podać poprawną liczbę całkowitą.");
 }
