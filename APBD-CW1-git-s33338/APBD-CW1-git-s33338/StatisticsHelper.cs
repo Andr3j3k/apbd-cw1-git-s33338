5ae6188120527e63b6412fe0cfff91dacbd2e60f
s33338 <s33338@pjwstk.edu.pl>
2026-03-12 13:41:13 +0100

    Add method CalculateAverage

--- APBD-CW1-git-s33338/APBD-CW1-git-s33338/StatisticsHelper.cs
+++ APBD-CW1-git-s33338/APBD-CW1-git-s33338/StatisticsHelper.cs
@@ -11,4 +11,20 @@ public static class StatisticsHelper
     {
         return number * number * number;
     }
+    
+    public static double CalculateAverage(int[] values)
+    {
+        if (values == null || values.Length == 0)
+        {
+            return 0;
+        }
+ 
+        int sum = 0;
+ 
+        foreach (int value in values)
+        {
+            sum += value;
+        }
+        return (double)sum / values.Length;
+    }
 }
