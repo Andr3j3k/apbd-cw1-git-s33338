1f50d2214df85ec4fed79ef27bc5f176b1c4f6b9
s33338 <s33338@pjwstk.edu.pl>
2026-03-12 13:28:03 +0100

    Dodanie klasy

---
+++ APBD-CW1-git-s33338/APBD-CW1-git-s33338/StatisticsHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace APBD_CW1_git_s33338;
+
+public static class StatisticsHelper
+{
+    public static int Square(int number)
+    {
+        return number * number;
+    }
+ 
+    public static int Cube(int number)
+    {
+        return number * number * number;
+    }
+}
